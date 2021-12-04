9a4e415ba1d8a25ac6bab18499a8cf6f05211611
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-04 23:57:18 +0100

    feat: added model, routes & services

--- app/app.js
+++ app/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const router = require('./routes/phones');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/phones', router);
+
+app.use('*', (req,res,next) => {
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error)
+})
+app.use((error, req, res, next) => {
+  return res.status(error.status || 500).json(error.message || 'Unexpected error')
+})
+
+
 
 module.exports = app;
---
+++ app/routes/phones.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  getAllPhones, 
+  getPhone, 
+  postPhone, 
+  deletePhone,
+  updatePhone 
+} = require('../controllers/PhoneController');
+
+router.get('/', getAllPhones)
+      .get('/:id', getPhone)
+      .post('/', postPhone)
+      .delete('/:id', deletePhone)
+      .put('/:id', updatePhone);
+
+module.exports = router;
