b8010b34a730c866b6c37e45f4e735475c191080
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-10 18:04:28 +0100

    feat: added test db for local & testing

--- app/config/config.js
+++ app/config/config.js
@@ -1,12 +1,5 @@
-const {
-  MONGO_USERNAME,
-  MONGO_PASSWORD,
-  MONGO_HOSTNAME,
-  MONGO_DB
-} = process.env
-
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}`,
+  DB: process.env.DB || 'mongodb://localhost:27017',
   COLLECTION: 'phones'
 }
