ebe4c24836c8670156994d00fe32488d95b6d80d
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-11 21:18:10 +0100

    fix: remove console.log for debugging

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,3 @@
-console.log('process.env.DB', process.env.MONGO_DB_STRING)
-
 module.exports = {
   PORT: process.env.PORT || 3000,
   DB: process.env.MONGO_DB_STRING || 'mongodb://localhost:27017',
