f950193f17113ea96168e3a4003044a2c2debbae
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-11 21:11:55 +0100

    fix: undefined string db

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
-console.log('process.env.DB', process.env.DB)
+console.log('process.env.DB', process.env.MONGO_DB_STRING)
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB: process.env.DB || 'mongodb://localhost:27017',
+  DB: process.env.MONGO_DB_STRING || 'mongodb://localhost:27017',
   COLLECTION: 'phones'
 }
