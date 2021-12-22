ccc93d5e712fd209f1cfd7176c5d4d1b61ee0e65
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-22 17:07:35 +0100

    feat: modified port for local (not docker one)

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 3002,
   DB: process.env.MONGO_DB_STRING || 'mongodb://localhost:27017',
   COLLECTION: 'phones'
 }
