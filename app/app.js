3265e30255a9081c1f7c36448928809c623989a3
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-12-23 13:07:04 +0100

    feat: allow cors

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const router = require('./routes/phones')
+const cors = require('cors')
 
+const router = require('./routes/phones')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
